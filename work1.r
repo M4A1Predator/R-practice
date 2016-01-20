0c39f2ad57d96ad60fad4c29401de2eef95d188f
M4A1Predator <tssniper3@gmail.com>
2016-01-20 23:35:06 +0700

    Update R files
    Standardization, Categorization

--- work1.r
+++ work1.r
@@ -41,11 +41,39 @@ boxplot(iData[,1], main="Duration before outlier treatment", range=0, horizontal
 #after
 boxplot(newData[,1], main="Duration after outliers treatment", range=0, horizontal=TRUE)
 
+summary(iData)
+summary(newData)
+
+# Standardization
+par(mfrow=c(1,1))
+metrix <- scale(newData[,1:2])
+boxplot(metrix, range=0, horizontal=TRUE, main="Standardization")
+
+summary(metrix)
+#dotchart(iData[1:20,2], labels = row.names(iData))
+
+# Categorization
+#print(metrix[1:10,2])
+par(mfrow=c(1,2))
+cate1 <- findInterval(metrix[,1], c(-2,-1,0,1,2,3))
+summary(cate1)
+cate2 <- findInterval(metrix[,2], c(-2,-1,0,1,2,3))
+summary(cate2)
+dotchart(cate1, main = "Dur")
+dotchart(cate2, main = "CreA")
+
+myInterval <- cut(metrix[1:10,2], c(-2,-1,0,1,2,3))
+print(myInterval)
 
 
 #count <- table(newData[,2])
 #barplot(count, main="Bar plot CreA", xlab="amount", horiz=TRUE)
 #barplot(table(iData[,2]), main="Bar plot CreA", xlab="amount", horiz=TRUE)
 
-
+"
+ref = http://www.r-bloggers.com/centering-and-standardizing-dont-confuse-your-rows-with-your-columns/
+ref = http://stackoverflow.com/questions/12979456/r-code-to-categorize-age-into-group-bins-breaks
+ref = http://stackoverflow.com/questions/26103140/how-can-i-create-a-custom-colour-scale-using-ggplot2-and-geom-tile
+ref = http://stackoverflow.com/questions/6774339/r-how-do-i-put-two-box-plots-next-to-each-other-keeping-same-y-range-for-both
+"
 
